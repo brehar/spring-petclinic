55948743c9031dfa522abfa91c770bf11169edf6
Brett Hartman <brett.hartman@mac.com>
2019-05-20 14:17:16 -0700

    Refactor to multimodule Maven project

--- pet-clinic-web/src/main/java/me/bretthartman/springpetclinic/SpringPetclinicApplication.java
+++ pet-clinic-web/src/main/java/me/bretthartman/springpetclinic/SpringPetclinicApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringPetclinicApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SpringPetclinicApplication.class, args);
   }
-
 }
--- pet-clinic-web/src/test/java/me/bretthartman/springpetclinic/SpringPetclinicApplicationTests.java
+++ pet-clinic-web/src/test/java/me/bretthartman/springpetclinic/SpringPetclinicApplicationTests.java
@@ -8,9 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringPetclinicApplicationTests {
-
   @Test
-  public void contextLoads() {
-  }
-
+  public void contextLoads() {}
 }
